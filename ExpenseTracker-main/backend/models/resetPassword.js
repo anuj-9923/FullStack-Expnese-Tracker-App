9d9313c0e7c10d82bc58544ac3becd829b1bc509
Anuj Kumar <anujkumar0799@gmail.com>
2023-12-26 14:35:20 +0530

    Report generated feature created

--- ExpenseTracker-main/backend/models/resetPassword.js
+++ ExpenseTracker-main/backend/models/resetPassword.js
@@ -1,16 +1,18 @@
 const Sequelize = require('sequelize')
+
 const sequelize = require('../util/db')
 
-const resetPassword = sequelize.define('resetPassword',{
-    id :{
-        type : Sequelize.UUID,
-        defaultValue : Sequelize.UUIDV4,
-        primaryKey : true
+const resetPassword = sequelize.define('resetPassword', {
+    id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
     },
-    isActive : {
-        type : Sequelize.BOOLEAN,
-        defaultValue : true
+    isActive: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
     }
 })
+
 
 module.exports = resetPassword;
