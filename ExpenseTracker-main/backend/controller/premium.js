64fea604326d7a809f9066b57253fb33e64a4084
Anuj Kumar <anujkumar0799@gmail.com>
2023-12-26 10:19:19 +0530

    premium feature inbuilt

--- ExpenseTracker-main/backend/controller/premium.js
+++ ExpenseTracker-main/backend/controller/premium.js
@@ -3,44 +3,40 @@ const Sequelize = require('sequelize')
 const Expense = require('../models/expense')
 const User = require('../models/user')
 
-
-
-
-exports.checkPremium = async(req ,res)=>{
-    try{
+exports.checkPremium = async (req, res) => {
+    try {
 
         const result = await req.user.isPremiumUser;
         return res.json(result)
-    }catch(e){
+    } catch (e) {
         console.log(e)
-        return res.status(500).json({success : false , msg :"Internal server error"})
-    
+        return res.status(500).json({ success: false, msg: "Internal server error" })
+
     }
 }
 
-
-exports.showLeaderBoard = async(req,res)=>{
-    try{
+exports.showLeaderBoard = async (req, res) => {
+    try {
         // return res.json(req.user)
-        if(req.user.isPremiumUser){
+        if (req.user.isPremiumUser) {
 
-        
-        const result = await User.findAll({
-            attributes :[
-                'id',
-                'name',
-               'totalAmount'
-            ],
-            order : [['totalAmount', 'DESC']]
-        })
-        console.log(result)
-        return res.json(result)
-    }else{
-        return res.status(403).json({success : false , msg :"you are not a premium user"})
 
-    }
-    }catch(e){
+            const result = await User.findAll({
+                attributes: [
+                    'id',
+                    'name',
+                    'totalAmount'
+                ],
+                order: [['totalAmount', 'DESC']]
+            })
+            console.log(result)
+            return res.json(result)
+        } else {
+            return res.status(403).json({ success: false, msg: "you are not a premium user" })
+
+        }
+    } catch (e) {
         console.log(e)
-        return res.status(500).json({success : false , msg :"Internal server error"})
+        return res.status(500).json({ success: false, msg: "Internal server error" })
     }
 }
